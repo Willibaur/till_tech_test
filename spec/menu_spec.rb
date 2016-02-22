c59197df489fe13f3006874466fecb795ee45775
Daniela <daniela.galeote@gmail.com>
2016-02-22 14:52:00 +0000

    Extracting menu items from json menu file

--- spec/menu_spec.rb
+++ spec/menu_spec.rb
@@ -4,6 +4,6 @@ describe Menu do
   subject(:menu) {described_class.new}
 
   it 'returns items and prices from menu' do
-    expect(menu.list("Cafe Latte")).to eq 4,75
+    expect(menu.list("Cortado")).to eq 4.55
   end
 end
